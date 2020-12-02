a55064443ce8a4748e43fc2daf6cf809e5c06c72
lucwhite <lw1944@nyu.edu>
2020-12-02 14:38:09 -0500

    Port Changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var socket = require('socket.io');
 var grid = [];
 
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 3000);
 var io = socket(server);
 
 app.use(express.static('public'));
